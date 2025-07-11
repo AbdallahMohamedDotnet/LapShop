be5c5b982ca37a0e17a9f14f046090a1c21122b6
Abdallah mohamed <160553826+Abdallahwww@users.noreply.github.com>
2025-07-11 06:06:07 +0300

    update

--- Areas/admin/Controllers/ItemsController.cs
+++ Areas/admin/Controllers/ItemsController.cs
@@ -1,8 +1,10 @@
 ﻿using LapShopv2.BL;
 using LapShopv2.Models;
 using LapShopv2.Utlities;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
 namespace LapShopv2.Areas.admin.Controllers
 {
     [Area("admin")]
@@ -10,21 +12,23 @@ namespace LapShopv2.Areas.admin.Controllers
     {
         MyDbContext context = new MyDbContext();
         ClsItem oClsItem = new ClsItem(new MyDbContext());
-        ClsCategories oClsCategories  = new ClsCategories();
+        ClsCategories oClsCategories = new ClsCategories();
         ClsItemTypes oClsItemType = new ClsItemTypes();
         ClsOs oClsOs = new ClsOs();
+
         public IActionResult List()
         {
             ViewBag.lstCategories = oClsCategories.GetAll();
             var items = oClsItem.GetAllItemData(null);
             return View(items);
         }
+
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Save(TbItem Item, List<IFormFile> Files)
         {
             //if (!ModelState.IsValid)
-            //    return View("Edit", category);
+            //    return View("Edit", Item);
             Item.ImageName = await Helper.UploadImage(Files, "Items");
             oClsItem.Save(Item);
             return RedirectToAction("List");
@@ -36,14 +40,16 @@ namespace LapShopv2.Areas.admin.Controllers
             var items = oClsItem.GetAllItemData(id);
             return View("List", items);
         }
+
         public IActionResult Delete(int itemId)
         {
             oClsItem.Delete(itemId);
             return RedirectToAction("List");
         }
+
         public IActionResult Edit(int? itemId)
         {
-            var item = new Models.TbItem();
+            var item = new TbItem();
             ViewBag.lstCategories = oClsCategories.GetAll();
             ViewBag.lstItemTypes = oClsItemType.GetAll();
             ViewBag.context = new MyDbContext();
