c8c292146d7aabf21867d68ca33ddb2e60d73564
Abdallah mohamed <160553826+Abdallahwww@users.noreply.github.com>
2025-07-12 05:36:41 +0300

    Initial commit with LapShop, BL, and Domain projects

---
+++ Domains/TBSettings.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LapShopv2.Models
+{
+    public class TBSettings
+    {
+        public int id { get; set; }
+        public string webSiteName { get; set; }
+        public string Logo { get; set; }
+        public string WebSiteDescription { get; set; }
+        public string FaceBookLink { get; set; }
+        public string instegramLink { get; set; }
+        public string YoutubeLink { get; set; }
+        public string WhatsAppLink { get; set; }
+
+    }
+}
