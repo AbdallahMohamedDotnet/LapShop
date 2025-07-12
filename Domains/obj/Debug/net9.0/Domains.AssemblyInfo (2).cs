8d2ac61431d364190c60b6f99b70641963acd4dc
Abdallah mohamed <160553826+Abdallahwww@users.noreply.github.com>
2025-07-12 06:00:57 +0300

    main file

--- Domains/obj/Debug/net9.0/Domains.AssemblyInfo (2).cs
+++ Domains/obj/Debug/net9.0/Domains.AssemblyInfo (2).cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domains")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e10f01e7cf63b3c83a27a2c18ff4f3e921ebda7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8c292146d7aabf21867d68ca33ddb2e60d73564")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domains")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domains")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
