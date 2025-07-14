0b6756148db1540c2860b0bf307a1b4925c49037
Abdallah mohamed <160553826+Abdallahwww@users.noreply.github.com>
2025-07-14 03:20:30 +0300

    ItemDetails

---
+++ BL/ItemImage.cs
@@ -0,0 +1,35 @@
+﻿using BL.Icontract;
+using LapShopv2.BL.Icontract;
+using LapShopv2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace BL
+{
+    public class ItemImage
+    {
+        public class ClsItemImages : IItemImages
+        {
+            MyDbContext context;
+            public ClsItemImages(MyDbContext ctx)
+            {
+                context = ctx;
+            }
+
+            public List<TbItemImage> GetByItemId(int id)
+            {
+                try
+                {
+                    var item = context.TbItemImages.Where(a => a.ItemId == id).ToList();
+                    return item;
+                }
+                catch
+                {
+                    return new List<TbItemImage>();
+                }
+            }
+        }
+    }
+}
--- BL/obj/Debug/net9.0/BL.AssemblyInfo.cs
+++ BL/obj/Debug/net9.0/BL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d2ac61431d364190c60b6f99b70641963acd4dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0049b096203ea94f7c1e1b1129de8013fdf8836")]
 [assembly: System.Reflection.AssemblyProductAttribute("BL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
