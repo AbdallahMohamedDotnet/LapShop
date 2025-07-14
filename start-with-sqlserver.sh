1a7590e25203ee9d5eee2189a0a1d99efecc35b7
Abdallah mohamed <160553826+Abdallahwww@users.noreply.github.com>
2025-07-14 03:36:23 +0300

    docker sql update setting

--- start-with-sqlserver.sh
+++ start-with-sqlserver.sh
@@ -1,15 +1,15 @@
-#!/bin/bash
-#set -e
+!/bin/bash
+set -e
 
-# Start SQL Server in the background
-#/opt/mssql/bin/sqlservr &
+ Start SQL Server in the background
+/opt/mssql/bin/sqlservr &
 
-# Wait for SQL Server to be available
-#for i in {1..30}; do
-  #/opt/mssql-tools/bin/sqlcmd -S abdallah -U sa -P "Your_password123" -Q "SELECT 1" && break
-  #echo "Waiting for SQL Server to start... ($i)"
- # sleep 2
-#done
+ Wait for SQL Server to be available
+for i in {1..30}; do
+  /opt/mssql-tools/bin/sqlcmd -S abdallah -U sa -P "Your_password123" -Q "SELECT 1" && break
+  echo "Waiting for SQL Server to start... ($i)"
+  sleep 2
+done
 
-# Run the .NET app
-#exec dotnet LapShopv2.dll
+ Run the .NET app
+exec dotnet LapShopv2.dll
